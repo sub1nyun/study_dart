accda8a24ba42b226a412db8093397ceb1e7924f
sub1nyun <weirdw4ve@gmail.com>
2023-02-03 18:26:34 +0900

    입력 받는 거 왜 안 되냐고!!!

--- 중간 정리/변수.dart
+++ 중간 정리/변수.dart
@@ -1,1 +1,12 @@
-// class를 제외하고 기본적으로 메모리에 로딩됨
+import 'dart:io';
+
+main() {
+  print("Hello World");
+  int a = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!);
+  print(a + b);
+}
+
+int add(int a, int b) {
+  return a + b;
+}
